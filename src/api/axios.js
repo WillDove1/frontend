43fa8c48652b2da4d4ffe126f679c5c48a521ed8
Javier_Laptop <javierisc.27@gmail.com>
2023-12-02 20:32:05 -0600

    cambiando axios con la url con /api

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: 'http://localhost:4000/api',
-    baseURL: 'https://apiproductos-dovt.onrender.com',
+    baseURL: 'https://apiproductos-dovt.onrender.com/api',
     withCredentials: true,
     headers:{
         Accept: 'application/json'
