7411f8848ca30ef29dfa005539ce924e90909221
Javier_Laptop <javierisc.27@gmail.com>
2023-12-02 19:46:53 -0600

    modificando axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: 'http://localhost:4000/api',
-    baseURL: 'https://apiproductos-dj0d.onrender.com',
+    baseURL: 'https://apiproductos-dovt.onrender.com',
     withCredentials: true,
     headers:{
         Accept: 'application/json'
