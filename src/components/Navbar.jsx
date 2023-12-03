4a0c3cbb8c95910aaf19f75db13ec3ccdbe26b80
Javier_Laptop <javierisc.27@gmail.com>
2023-12-03 16:55:43 -0600

    Arreglando problema con salir

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,7 +85,7 @@ function Navbar(){
             <Menu.Item>
               {({ active }) => (
                 <Link
-                  to="/"
+                  to="/logout"
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm'
