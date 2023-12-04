ee47c36d53a25c6ca2d1dbaf0a2a258c981f28b5
Javier_Laptop <javierisc.27@gmail.com>
2023-12-03 19:10:16 -0600

    Provider intento 4

--- src/pages/ProviersFormPage.jsx
+++ src/pages/ProviersFormPage.jsx
@@ -22,9 +22,9 @@ function ProviersFormPage() {
         async function loadProvider(){
             if(params.id){
                 const provider = await getProvider(params.id);
-                setValue('productName',provider.productName);
-                setValue('price',provider.price);
                 setValue('name',provider.name);
+                setValue('nameProduct',provider.nameProduct);
+                setValue('price',provider.price);
                 
             }
         }
@@ -46,16 +46,28 @@ function ProviersFormPage() {
         <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
         <form onSubmit={onSubmit}>
             <h1 className='text-3xl font-bold my-2'>Proveedores</h1>
-            <label htmlFor="nameProduct">Nombre del producto</label>
+            <label htmlFor="name">Nombre del proveedor</label>
             <input type="text"
                 className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
-                placeholder='Nombre del producto' 
+                placeholder='Nombre del proveedor' 
                 {
-                    ...register("nameProduct", {required:true})
+                    ...register("name", {required:true})
                 }
                 autoFocus
             />
-            { errors.nameProduct && (
+            { errors.name && (
+                <div className='text-red-500'>Nombre del proveedor es requerido</div>
+            )}
+
+            <label htmlFor="nameProduct">Nombre del producto</label>
+            <input type="text"
+                className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                placeholder='Nombre del producto'
+                {
+                    ...register("nameProduct", {required:true})
+                }
+            />
+            { errors.name && (
                 <div className='text-red-500'>Nombre del producto es requerido</div>
             )}
 
@@ -79,18 +91,6 @@ function ProviersFormPage() {
             )
 
             }
-
-            <label htmlFor="name">Proveedor</label>
-            <input type="text"
-                className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
-                placeholder='Nombre del proveedor'
-                {
-                    ...register("name", {required:true})
-                }
-            />
-            { errors.name && (
-                <div className='text-red-500'>Nombre del proveedor es requerido</div>
-            )}
 
             <button className='bg-zinc-700 px-3 py-3 rounded-md' 
             type='submit'
