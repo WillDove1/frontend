0230f8eac28c9af425025bb71904c9ea123e8518
Javier_Laptop <javierisc.27@gmail.com>
2023-12-03 22:02:36 -0600

    Resolviendo error en context con employees.js

--- src/api/employees.js
+++ src/api/employees.js
@@ -3,7 +3,7 @@ import axios from './axios';
 export const getEmployeesRequest = () => axios.get('/employees');
 export const getEmployeeRequest = (id) =>axios.put('/employees/'+id);
 
-export const createEmployeesRequest = (employee) => axios.post('/employees',employee);
+export const createEmployeeRequest = (employee) => axios.post('/employees',employee);
 
 export const deleteEmployeeRequest = (id) =>axios.delete('/employees/' + id);
 export const updateEmployeeRequest = (id,employee) =>axios.put('/employees/' + id, employee);
